a695a7bc7cf4209a554e6987b3b830799fbfc0f4
Stewart Wingfield <sww915@gmail.com>
2023-07-27 20:01:29 -0400

    added more variables

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 class bankAccount {
-    constructor(accountNumber, owner, transactions)
+    transactions = []
+    constructor(accountNumber, owner) {
+        this.accountNumber = accountNumber
+        this.owner = owner 
+    }
+    balance() {
+
+    }
+    deposit(amt) {
+
+    }
+    charge(payee, amt) {
+
+    }
 }
+
+class transaction {
+    date = new Date().toLocaleDateString()
+    constructor(amount, payee)
+}
+
+console.log(date)
