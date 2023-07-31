6342f6d8bcf9fc5fbfb7a882a8eda1c4bba30d48
Stewart Wingfield <sww915@gmail.com>
2023-07-31 19:12:47 -0400

    final product

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 class bankAccount {
-    transactions = []
     constructor(accountNumber, owner) {
         this.accountNumber = accountNumber
         this.owner = owner 
+        this.transactions = []
     }
     balance() {
-
+        let newBalance = 0
+        for (let i = 0; i < this.transactions.length; i++) {
+           newBalance += this.transactions[i].amount
+        } return newBalance
     }
     deposit(amt) {
-
+        if (amt > 0) {
+            let money = new transaction(amt, this.owner)
+            this.transactions.push(money)
+        }
     }
     charge(payee, amt) {
-
+        let totalBalance = this.balance()
+        if (amt < totalBalance) {
+            let chargeTransaction = new transaction(-amt, payee)
+            this.transactions.push(chargeTransaction)
+        }
     }
 }
 
 class transaction {
-    date = new Date().toLocaleDateString()
-    constructor(amount, payee)
+    constructor(amount, payee){
+        this.amount = amount
+        this.payee = payee
+        this.date = Date()
+    }
 }
 
-console.log(date)
+ let newBankAccount = new bankAccount('1', '2')
+console.log(newBankAccount) 
+
+console.log(newBankAccount)
+
+newBankAccount.deposit(100) 
+console.log(newBankAccount)
+
+newBankAccount.charge('target', 50)
+console.log(newBankAccount)
+
+console.log(newBankAccount.balance())
